45b838e5f36c45d8f36b16895c845cadc6d7d5c1
Keren <kerenlopezcordoba@outlook.es>
2023-05-13 22:48:12 -0500

    The deployment of the database was performed

--- scripts/postgres/coffeemach-user.sql
+++ scripts/postgres/coffeemach-user.sql
@@ -1,6 +1,6 @@
-create user cofmachu with password 'cofmachpwd';
-create database coffeemachine owner cofmachu;
-grant connect on database coffeemachine to cofmachu;
+create user cofmachkalls with password 'pwd';
+create database coffeemachine owner cofmachkalls;
+grant connect on database coffeemachine to cofmachkalls;
 
 -- Connection
 -- psql -d coffeemachine -U p09713_1_2
